c7c42cdd2cd6a1678e875aea965bd5fa2033b572
adewalejnr2 <sherifadewale671@yahoo.com>
2024-05-27 14:24:07 -0400

    my upload project

---
+++ app.js
@@ -0,0 +1,11 @@
+const openEl = document.querySelector('.open-btn');
+const closeEl = document.querySelector('.close-btn');
+const sidebarEl = document.querySelector('.sidebar');
+
+openEL.addEventListener('click', function () {
+  sidebarEl.classList.add('open');
+});
+
+closeEl.addEventListener('click', function () {
+  sidebarEl.classList.remove('open');
+});
