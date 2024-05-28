6be2e2a5181ee89f8af62e3548c415c1274add39
adewalejnr2 <sherifadewale671@yahoo.com>
2024-05-28 10:05:42 -0400

    my updated project

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-//hello world 
+//hello world
 const openEl = document.querySelector('.open-btn');
 const closeEl = document.querySelector('.close-btn');
 const sidebarEl = document.querySelector('.sidebar');
 
-openEL.addEventListener('click', function () {
+openEl.addEventListener('click', function () {
   sidebarEl.classList.add('open');
 });
 
 closeEl.addEventListener('click', function () {
   sidebarEl.classList.remove('open');
 });
+console.log('hello');
